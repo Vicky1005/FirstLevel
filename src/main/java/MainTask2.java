143d376f2e464c593f9f41668e6784c7932bfab2
Victoria Tatarinova <victoria.tatarinova10@gmail.com>
2019-12-06 14:55:30 +0300

    optional task - 2

--- src/main/java/MainTask2.java
+++ src/main/java/MainTask2.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class MainTask2 {
 
     public static void main(String[] args) {
-        System.out.println("Enter integer numbers");
-        Scanner scan = new Scanner(System.in);
-        int[] numbers = new int[5];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = scan.nextInt();
-        }
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            System.out.println(numbers[i]);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]:" + args[i]);
         }
     }
 }
--- src/main/java/OptionalTaskFirst1.java
+++ src/main/java/OptionalTaskFirst1.java
@@ -4,18 +4,70 @@ public class OptionalTaskFirst1 {
     public static void main(String[] args) {
         System.out.println("Enter integer numbers");
         Scanner scan = new Scanner(System.in);
-        int number1=scan.nextInt();
-        int number2=scan.nextInt();
-        int number3=scan.nextInt();
-
-
-        int counter=0;
-        while (number1>0) {
-            number1=number1%10;
-            number1=number1/10;
-            counter++;
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scan.nextInt();
         }
 
-        System.out.println(counter);
+        // 1.     Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
+        int count = 0;
+        int temporary = 0;
+        int[] numbersLenght = new int[numbers.length];
+        for (int i = 0; i < numbersLenght.length; i++) {
+            temporary = numbers[i];
+            while (temporary != 0) {
+                temporary /= 10;
+                count++;
+                numbersLenght[i] = count;
+            }
+            count = 0;
+        }
+
+        int maxLenght = (int) Double.NEGATIVE_INFINITY;
+        int maxNumber = numbers[0];
+        for (int i = 0; i < numbersLenght.length; i++) {
+            if (maxLenght < numbersLenght[i]) {
+                maxLenght = numbersLenght[i];
+                maxNumber = numbers[i];
+            }
+
+        }
+
+        int minLenght = (int) Double.POSITIVE_INFINITY;
+        int minNumber = numbers[0];
+        for (int i = 0; i < numbersLenght.length; i++) {
+            if (minLenght > numbersLenght[i]) {
+                minLenght = numbersLenght[i];
+                minNumber = numbers[i];
+            }
+
+        }
+        System.out.println("The longest number is " + maxNumber + " - contains " + maxLenght + " chars");
+        System.out.println("The shortest number is " + minNumber + " - contains " + minLenght + " chars");
+
+
+        //3.     Вывести на консоль те числа, длина которых меньше (больше) средней длины по всем числам, а также длину.
+
+        int sum = 0;
+        for (int i = 0; i < numbersLenght.length; i++) {
+            sum += numbersLenght[i];
+        }
+
+        int averageLenght = sum / numbersLenght.length;
+
+        for (int i = 0; i < numbersLenght.length; i++) {
+            if (numbersLenght[i] < averageLenght) {
+                System.out.println("The lenght of number " + numbers[i] + " is less then average. It's equal to " + numbersLenght[i]);
+            }
+        }
+
+        for (int i = 0; i < numbersLenght.length; i++) {
+            if (numbersLenght[i] > averageLenght) {
+                System.out.println("The lenght of number " + numbers[i] + " is more then average. It's equal to " + numbersLenght[i]);
+            }
+        }
+
     }
+
 }
+
